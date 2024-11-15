82ab34fe1b09d509a1585cef2e92c2af3b6bf5f2
John-Thailand <naoki.honda@mirrorfit.jp>
2024-11-15 12:22:21 +0900

    自己紹介アプリのリファクタリング

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -4,39 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.jetprofile.components.CompanySection
+import com.example.jetprofile.components.DetailSection
 import com.example.jetprofile.ui.theme.JetProfileTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,101 +46,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.padding(20.dp)
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.img_profile),
-                            contentDescription = "プロフィール",
-                            modifier = Modifier
-                                .size(100.dp)
-                                .clip(RoundedCornerShape(10.dp))
-                        )
-                        
-                        Spacer(modifier = Modifier.height(20.dp))
-
-                        Text(
-                            text = "本田 尚記",
-                            color = Color.Gray,
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.ExtraBold,
-                        )
-
-                        Spacer(modifier = Modifier.height(20.dp))
-
-                        Text(
-                            text = "Flutterエンジニア",
-                            color = Color.Gray,
-                            fontSize = 16.sp,
-                        )
-                        
-                        Spacer(modifier = Modifier.height(20.dp))
-
-                        Column(
-                            horizontalAlignment = Alignment.Start,
-                            modifier = Modifier.fillMaxWidth(),
-                        ) {
-                            Text(
-                                text = "StarryBase LLC",
-                                fontSize = 26.sp,
-                                fontWeight = FontWeight.Bold,
-                            )
-                            Spacer(modifier = Modifier.height(10.dp))
-                            Text(
-                                text = "開発部",
-                                color = Color.Gray,
-                                fontSize = 16.sp,
-                            )
-                            Spacer(modifier = Modifier.height(20.dp))
-
-                            Label(icon = Icons.Default.Email, text = "Email")
-
-                            Spacer(modifier = Modifier.height(10.dp))
-                            Text(
-                                text = "example@gmail.com",
-                                fontSize = 16.sp,
-                            )
-                            Spacer(modifier = Modifier.height(5.dp))
-
-                            Divider(
-                                modifier = Modifier.clip(RoundedCornerShape(1000.dp)),
-                                thickness = 2.dp,
-                            )
-                        }
-                        
-                        Spacer(modifier = Modifier.height(20.dp))
-                        
-                        Button(
-                            modifier = Modifier.fillMaxWidth(),
-                            colors = ButtonDefaults.buttonColors(Color(0xFFF85F6A)),
-                            onClick = { /*TODO*/ },
-                        ) {
-                            Text(text = "詳細を表示", color = Color.White)
-                        }
-
-                        Spacer(modifier = Modifier.height(20.dp))
-
-                        Column(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .clip(RoundedCornerShape(10.dp))
-                                .background(Color.LightGray.copy(alpha = 0.3f))
-                                .padding(horizontal = 10.dp, vertical = 20.dp)
-                        ) {
-                            Label(
-                                icon = Icons.Default.Favorite,
-                                text = "趣味：お散歩",
-                                color = Color.Gray,
-                            )
-                            Spacer(modifier = Modifier.height(10.dp))
-                            Label(
-                                icon = Icons.Default.LocationOn,
-                                text = "居住地：福岡市東区",
-                                color = Color.Gray,
-                            )
-                        }
-                    }
+                    MainContent()
                 }
             }
         }
@@ -151,18 +54,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Label(icon: ImageVector, text: String, color: Color = MaterialTheme.colorScheme.onBackground) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Icon(
-            imageVector = icon,
-            contentDescription = null,
+fun MainContent() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(20.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_profile),
+            contentDescription = "プロフィール",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(RoundedCornerShape(10.dp))
         )
-        Spacer(modifier = Modifier.width(10.dp))
+
+        Spacer(modifier = Modifier.height(20.dp))
+
         Text(
-            text = text,
-            color = color,
-            fontSize = 14.sp,
-            fontWeight = FontWeight.Bold,
+            text = "本田 尚記",
+            color = Color.Gray,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.ExtraBold,
         )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "Flutterエンジニア",
+            color = Color.Gray,
+            fontSize = 16.sp,
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        CompanySection()
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // 詳細表示ボタン
+        // rememberでメモリ内に変数を記憶する
+        var isShowDetail by remember { mutableStateOf(false) }
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(Color(0xFFF85F6A)),
+            onClick = { isShowDetail = !isShowDetail },
+        ) {
+            Text(text = "詳細を表示", color = Color.White)
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        if (isShowDetail) {
+            DetailSection()
+        }
     }
 }
